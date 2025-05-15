91d6dcee62aed11531c1ef94f7853750f89ec0db
Coretava Developer <developer@Coretavas-MacBook-Pro.local>
2025-05-15 14:46:02 +0300

    from main

--- myfistcode.tsx
+++ myfistcode.tsx
@@ -1,6 +1,7 @@
 import button from '';
 function MyButton() {
     return (
-      <button>I'm a button</button>
+      <button>I'm a button1</button>
+      <button>I'm a button2</button>
     );
   }
