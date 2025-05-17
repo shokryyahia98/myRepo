7a2e1dc733ffa746e6e66eec360ae8f27fac62fc
Coretava Developer <developer@Coretavas-MacBook-Pro.local>
2025-05-17 13:44:50 +0300

    Added todo list feature

--- myfistcode.tsx
+++ myfistcode.tsx
@@ -1,6 +1,32 @@
-import button from '';
-function MyButton() {
-    return (
-      <button>I'm a button</>
-    );
-  }
+import React, { useState } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim() !== '') {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Todo List</h1>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter task"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, idx) => (
+          <li key={idx}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
